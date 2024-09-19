786f2b054f77f4123ebc1d6432c0808293d923e0
Miki <tamnhu03@gmail.com>
2024-09-19 13:52:45 +0700

    Data fetching API using SWR

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import { Background } from '@/app/home/components/background';
 import { BookItem } from '@/app/home/components/bookItem';
 import { Header } from '@/app/home/components/header';
 
-import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 export interface BookType {
     author: string;
@@ -17,49 +17,25 @@ export interface BookType {
 }
 
 const Home = () => {
-    const [bookList, setBookList] = useState<BookType[] | null>(null);
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-
-    const [search, setSearch] = useState("");
+    // const [search, setSearch] = useState("");
 
     // random price generator
     const generateRandomPrice = (min: number, max: number) => {
         return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
-    useEffect(() => {
-        const fetchBooks = async() => {
-            setIsLoading(true);
-            setIsError(false);
-            try {
-                const response = await fetch('https://freetestapi.com/api/v1/books');
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data:BookType[] = await response.json();
-                console.log('hi', data);
-                const bookList = data.map((book) => {
-                    const randomPrice = generateRandomPrice(100000, 500000);
-                    return {
-                        ...book,
-                        price: randomPrice
-                    };
-                });
-                setBookList(bookList);
+    const { data, error } = useSWR('https://freetestapi.com/api/v1/books', async (url) => {
+        const response = await fetch(url);
+        return response.json();
+    });
 
-                
-            } catch (e) {
-                setIsError(true);
-                if (e instanceof Error) setError(e.message);
-                else setError("Unknown error occurred");
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchBooks();
-    }, []);
+    const BookList = data?.map((book) => {
+        const randomPrice = generateRandomPrice(100000, 500000);
+        return {
+            ...book,
+            price: randomPrice,
+        };
+    });
     
     return (
         <>
@@ -67,7 +43,7 @@ const Home = () => {
             <Header />
             <div className="p-4">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                    {bookList?.map((book) => (
+                    {BookList?.map((book) => (
                         <BookItem key={book.id} book={book} />
                     ))}
                 </div>
