4da17adf2947e1413556ad77bae475dd226842dd
Miki <tamnhu03@gmail.com>
2024-09-20 18:05:56 +0700

    Modify UI for Homepage and each book display

--- src/app/home/components/bookItem.tsx
+++ src/app/home/components/bookItem.tsx
@@ -18,48 +18,50 @@ export const BookItem = (props: BookItemProps) => {
     useEffect(() => {
         setIsInWishList(wishList.some((item) => item.id === book.id));
     }, [wishList, book.id]);
+    
     const handleAddToCart = () => {
         dispatch(addToCart(book));
     };
 
     const handleAddToWishList = () => {
         if (isInWishList) {
-            // Remove from wishlist
-            
             setIsInWishList(false);
         } else {
-            // Add to wishlist
             dispatch(addToWishList(book));
             setIsInWishList(true);
         }
     };
 
+    const storedISBN = JSON.parse(localStorage.getItem('ISBN')) || {};
+    const ISBN = storedISBN[book?.id] || 0;
+
     return (
         <Link href={`/book/${book.id}`}>
-        <div className="bg-white rounded-lg shadow-md overflow-hidden relative">
-            <img
-                src={book.cover_image}
-                alt={book.title}
-                className="w-full h-72 object-cover mx-auto"
-            />
-            <button
-                className="absolute top-2 right-2"
-                onClick={handleAddToWishList}
-            >
+         <div className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col h-full relative">
                 <img
-                    src={isInWishList ? "/assets/red-heart.png" : "/assets/heart.png"}
-                    alt="wishlist"
-                    style={{ width: '30px', height: '30px' }}
+                    src={book.cover_image}
+                    alt='book cover'
+                    className="w-full h-72 object-cover"
                 />
-            </button>
-            <div className="p-4">
-                <h3 className="text-xl font-semibold mb-2">{book.title}</h3>
-                <p className="text-gray-700">Author: {book.author}</p>
-                <p className="text-gray-700">Year: {book.publication_year}</p>
-                <p className="text-gray-700">Description: {book.description}</p>
-                <p className="text-gray-700 font-bold pt-2 text-lg">{book.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
-
-                <div className="flex items-center justify-between mt-4">
+                <button
+                    className="absolute top-2 right-2"
+                    onClick={handleAddToWishList}
+                >
+                    <img
+                        src={isInWishList ? "/assets/red-heart.png" : "/assets/heart.png"}
+                        alt="wishlist"
+                        style={{ width: '30px', height: '30px' }}
+                    />
+                </button>
+                <div className="p-4 flex flex-col flex-grow">
+                    <h3 className="text-xl font-semibold mb-2">{book.title}</h3>
+                    <p className="text-gray-700">Author: {book.author}</p>
+                    <p className="text-gray-700">Year: {book.publication_year}</p>
+                    <p className="text-gray-700">Description: {book.description}</p>
+                    <p className="text-gray-700 font-bold pt-2 text-lg">{book.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}</p>
+                </div>
+                <div className="flex items-center justify-between p-4 border-t">
+                    <p className="text-gray-700">ISBN: {book.ISBN}</p>
                     
                     <button
                         className="ml-4 bg-blue-500 text-white text-lg px-3 py-1 rounded-full hover:bg-blue-700 flex items-center"
@@ -74,7 +76,6 @@ export const BookItem = (props: BookItemProps) => {
                     </button>
                 </div>
             </div>
-        </div>
         </Link>
     );
 };
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,12 +14,19 @@ export interface BookType {
     publication_year: number;
     title: string;
     price: number;
+    ISBN: number;
 }
 
 // random price generator
 const generateRandomPrice = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+// random ISBN generator
+const generateRandomISBN = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 100000) + min);
+};
+
 
 
 const Home = () => {
@@ -31,16 +38,25 @@ const Home = () => {
 
     const BookList = book?.map((book: BookType) => {
         const storedPrice = JSON.parse(localStorage.getItem('price')) || {};
+        const storedISBN = JSON.parse(localStorage.getItem('ISBN')) || {};
         // retrieve the price from local storage if it exists, if not generate a random price
         const randomPrice = storedPrice[book.id] || generateRandomPrice(100000, 500000);
+        // retrieve the ISBN from local storage if exists, if not generate a random price
+        const randomISBN = storedISBN[book.id] || generateRandomISBN(100000000000, 900000000000);
 
         if (!storedPrice[book.id]) {
             localStorage.setItem('price', JSON.stringify({ ...storedPrice, [book.id]: randomPrice }));
         }
+        
+        if (!storedISBN[book.id]) {
+            localStorage.setItem('ISBN', JSON.stringify({ ...storedISBN, [book.id]: randomISBN }));
+        }
 
+        console.log(randomISBN)
         return {
             ...book,
             price: randomPrice,
+            ISBN: randomISBN,
             };
         }
     );
