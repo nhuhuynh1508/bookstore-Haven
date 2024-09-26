0b8ec5f33bb0582ee9849b172a44ecd76b0efbc3
Miki <tamnhu03@gmail.com>
2024-09-26 11:37:43 +0700

    Refactoring

---
+++ src/app/home/components/bookProcessor.tsx
@@ -0,0 +1,33 @@
+import { BookType } from "@/app/type";
+
+// random price generator
+const generateRandomPrice = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// random ISBN generator
+const generateRandomISBN = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 100000) + min);
+};
+
+export const processedBook = (book: BookType) => {
+    const storedPrice = JSON.parse(localStorage.getItem('price')) || {};
+    const storedISBN = JSON.parse(localStorage.getItem('ISBN')) || {};
+    // retrieve the price from local storage if it exists, if not generate a random price
+    const randomPrice = storedPrice[book.id] || generateRandomPrice(100000, 500000);
+    // retrieve the ISBN from local storage if exists, if not generate a random price
+    const randomISBN = storedISBN[book.id] || generateRandomISBN(100000000000, 900000000000);
+
+    if (!storedPrice[book.id]) {
+        localStorage.setItem('price', JSON.stringify({ ...storedPrice, [book.id]: randomPrice }));
+    }
+    
+    if (!storedISBN[book.id]) {
+        localStorage.setItem('ISBN', JSON.stringify({ ...storedISBN, [book.id]: randomISBN }));
+    }
+    return {
+        ...book,
+        price: randomPrice,
+        ISBN: randomISBN,
+        };
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
         'bodoni_moda': ['Bodoni Moda'],
         'eb_garamond': ['EB Garamond']
       },
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
     },
   },
   plugins: [],
