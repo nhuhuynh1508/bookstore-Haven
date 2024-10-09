54c33dc8902c9f7aa1bf66eda533e0596bcb9b43
Miki <tamnhu03@gmail.com>
2024-10-09 14:07:32 +0700

    Fix BookID page

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
     const wishListTotalQuantity = wishListItems.length;
     
     return (
-    <div className="w-full bg-blue-300 h-16 sm:h-20 flex items-center px-1 justify-around">
+    <div className="w-full bg-blue-300 h-16 sm:h-20 flex items-center px-2 justify-between">
             <div className="flex-shrink-0">
                 <DrawerMenu />
             </div>
@@ -27,22 +27,22 @@ export const Header = () => {
                 <img
                     src="/assets/book-icon.png"
                     alt="icon"
-                    className="w-10 h-10 sm:w-16 sm:h-16 mr-8"
+                    className="w-10 h-10 sm:w-16 sm:h-16 xs:mr-10"
                 />
             </Link>
 
             <div className='flex flex-grow mx-2 justify-center max-w-1/2'><SearchBar /></div>
             
 
-            <div className="flex items-center space-x-2 sm:space-x-3">
+            <div className="flex items-center space-x-2">
                 <Link href="/shopping" className="relative inline-block">
                     <Badge badgeContent={cartTotalQuantity} color="primary" sx={{ '& .MuiBadge-badge': { fontSize: '0.7rem', minWidth: '18px', height: '18px' } }}>
-                        <ShoppingCartIcon color="action" sx={{ fontSize: 40, sm: { fontSize: 24 } }} />
+                        <ShoppingCartIcon color="action" sx={{ fontSize: 34, sm: { fontSize: 24 }, xs: { fontSize: 16 }  }} />
                     </Badge>
                 </Link>
                 <Link href="/wishlist" className="relative inline-block">
                     <Badge badgeContent={wishListTotalQuantity} color="primary" sx={{ '& .MuiBadge-badge': { fontSize: '0.7rem', minWidth: '18px', height: '18px' } }}>
-                        <FavoriteIcon color="action" sx={{ fontSize: 40, sm: { fontSize: 24 } }} />
+                        <FavoriteIcon color="action" sx={{ fontSize: 34, sm: { fontSize: 24 }, xs: { fontSize: 16 } }} />
                     </Badge>
                 </Link>
             </div>
