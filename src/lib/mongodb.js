a7c060d2e831c5202b3d44eaeb26da97b33244c9
Tsumiki <tamnhu03@gmail.com>
2024-10-14 13:39:02 +0700

    Fix build error
    - Disable ES-Lint rules

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,9 +1,10 @@
-// src/lib/mongodb.js
-
 import { MongoClient } from 'mongodb';
 
 let client;
 let clientPromise;
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 if (!process.env.MONGODB_URI) {
     throw new Error("Please add your MongoDB URI to .env.local");
