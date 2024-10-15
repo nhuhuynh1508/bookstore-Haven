82f290dfb8bec91aaedca32b9b2c1c70baaeb7e3
Miki <tamnhu03@gmail.com>
2024-10-15 15:14:45 +0700

    Log in using Github

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,11 +6,13 @@ import { useAppSelector } from "@/lib/hooks";
 import { SearchBar } from '@/app/home/components/searchBar';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Badge } from "@mui/material";
+import { Badge, Button } from "@mui/material";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from 'next/link';
 import DrawerMenu from "./drawerMenu";
 
 export const Header = () => {
+    const { data: session } = useSession();
     const cartItems = useAppSelector((state) => state.book.cart.cartItems);
     const wishListItems = useAppSelector((state) => state.book.wishlist.wishListItems);
     const cartTotalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
@@ -23,7 +25,7 @@ export const Header = () => {
             </div>
 
             <Link href="/" className="flex items-center">
-                <span className="text-slate-800 flex-shrink-0 text-2xl sm:text-4xl xs:text-sm font-eb_garamond top-0 font-bold">Book Haven</span>
+                <span className="text-blue-800 flex-shrink-0 text-2xl sm:text-4xl xs:text-sm font-pacifico top-0">Book Haven</span>
                 <img
                     src="/assets/book-icon.png"
                     alt="icon"
@@ -35,6 +37,8 @@ export const Header = () => {
             
 
             <div className="flex items-center space-x-2">
+                {session ? (
+                    <>
                 <Link href="/shopping" className="relative inline-block">
                     <Badge badgeContent={cartTotalQuantity} color="primary" sx={{ '& .MuiBadge-badge': { fontSize: '0.7rem', minWidth: '18px', height: '18px' } }}>
                         <ShoppingCartIcon color="action" sx={{ fontSize: 34, sm: { fontSize: 24 }, xs: { fontSize: 16 }  }} />
@@ -45,6 +49,45 @@ export const Header = () => {
                         <FavoriteIcon color="action" sx={{ fontSize: 34, sm: { fontSize: 24 }, xs: { fontSize: 16 } }} />
                     </Badge>
                 </Link>
+                </>
+                ) : (
+                    <p></p>
+                )}
+            </div>
+
+            <div className="flex items-center space-x-2">
+                {session ? (
+                    // Display user profile image if authenticated
+                    <>
+                        <img
+                            src={session.user?.image}
+                            alt="User Profile"
+                            className="w-10 h-10 rounded-full"
+                        />
+                    {/* <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={() => signOut()}
+                    >
+                        Logout
+                    </button> */}
+                    <Button
+                        variant="outlined"
+                        onClick={() => signOut()}
+                        size="small"
+                    >
+                        Log out
+                    </Button>
+                    </>
+                ) : (
+                    // Display Sign In button if not authenticated
+                    <Button
+                        variant="outlined"
+                        onClick={() => signIn()}
+                        size="small"
+                    >
+                        Sign in
+                    </Button>
+                )}
             </div>
         </div>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import StoreProvider from "./storeProvider";
+import Provider from "./provider";
 
 
 export const metadata: Metadata = {
@@ -38,9 +38,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </head>
       <body>
-        <StoreProvider>
-          {children}
-        </StoreProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
