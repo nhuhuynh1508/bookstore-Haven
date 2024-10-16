e26a3f05a92491cd84d5a3f9f880951d1db5018f
Miki <tamnhu03@gmail.com>
2024-10-16 12:03:31 +0700

    Hardcode log in using credential

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,41 @@
-import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
-export const { handlers, auth } = NextAuth({
-    providers: [
-        GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        })
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
 
+export const { handlers } = NextAuth({
+    providers: [
+    Credentials({
+        credentials: {
+            email: {
+                label: "Email",
+                type: "Email",
+                placeholder: "email@example.com",
+            },
+            password: {
+                label: "Password",
+                type: "Password",
+            },
+        },
+        async authorize(credentials) {
+            const guest = {email: "guest@example.com"}
+            const admin = {email: "admin@example.com"}
+            // check if user exists
+            if (credentials.email === guest.email && credentials.password === "guest") {
+                return guest
+            } else if (credentials.email === admin.email && credentials.password === "admin") {
+                return admin
+            }
+        }
+    }),
+    GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+    })
     ],
 })
