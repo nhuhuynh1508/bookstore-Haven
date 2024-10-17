557cc76f68e5cb5c050738b8b3d116babe038175
Miki <tamnhu03@gmail.com>
2024-10-17 15:45:56 +0700

    Fix build error

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,18 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(req) {
-    // set cors headers
-    const headers = new Headers();
-    // allow all origins
-    headers.set('Access-Control-Allow-Origin', '*');
-    // allow methods
-    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        return NextResponse.json({}, { status: 200, headers });
-    }
-
+export async function GET() {
     const books = [
         {
             id: 1,
