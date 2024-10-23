60ac2a6bc683fc45783cb80b4fd24720833d3a3a
Miki <tamnhu03@gmail.com>
2024-10-23 17:49:02 +0700

    Prevent fetch other pages when not authenticated

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -1,12 +1,24 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { Header } from '../components/header';
 // import components
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 import { Subheader } from '../components/subheader';
 import { Cart } from './components/cart';
 import { CartBackground } from './components/cartBackground';
 
 
 function ShoppingCart() {
+    const { status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        // If not authenticated, redirect to the login page
+        if (status === "unauthenticated") {
+            router.push('/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F'); // Adjust the path to your login page
+        }
+    }, [status, router]);
     return (
         <>
             <Header />
