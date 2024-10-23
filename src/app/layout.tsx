a158e88e16cf3f871a5855691218477f07a2c748
Miki <tamnhu03@gmail.com>
2024-10-23 17:54:58 +0700

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+        <link rel="icon" type="image/x-icon" href='/assets/book-favicon.png' />
         <link
           href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
           rel="stylesheet"
